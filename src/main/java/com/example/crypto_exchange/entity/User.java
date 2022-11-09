5d362c1c8e191775ef47c871ce9d4af4d130b551
aslanmagamaev <aslanmagamaev@gmail.com>
2022-11-09 22:34:32 +0300

    Entities added

---
+++ src/main/java/com/example/crypto_exchange/entity/User.java
@@ -0,0 +1,37 @@
+package com.example.crypto_exchange.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Table(name = "user")
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "surname")
+    private String surname;
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "credential_id")
+    private UserCredential userCredential;
+}
